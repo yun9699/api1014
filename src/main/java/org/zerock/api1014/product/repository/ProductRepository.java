748309e417ab0f6c370d83d30bae9860c2a46fa6
yun9699 <cha23215@daum.net>
2024-10-14 10:53:19 +0900

    1053

--- src/main/java/org/zerock/api1014/product/repository/ProductRepository.java
+++ src/main/java/org/zerock/api1014/product/repository/ProductRepository.java
@@ -2,8 +2,9 @@ package org.zerock.api1014.product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.api1014.product.domain.Product;
+import org.zerock.api1014.product.repository.search.ProductSearch;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductSearch {
 
 
 }
---
+++ src/main/java/org/zerock/api1014/product/repository/search/ProductSearchImpl.java
@@ -0,0 +1,22 @@
+package org.zerock.api1014.product.repository.search;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.zerock.api1014.product.domain.Product;
+
+@Log4j2
+public class ProductSearchImpl extends QuerydslRepositorySupport implements ProductSearch {
+
+    public ProductSearchImpl() {
+        super(Product.class);
+    }
+
+    @Override
+    public Page<Product> list(Product product, Pageable pageable) {
+
+        log.info("list ---------------!!");
+        return null;
+    }
+}
