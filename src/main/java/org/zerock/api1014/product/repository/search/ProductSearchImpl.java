05256ae26f6fbafa7e7909118eef107aea2e99b7
yun9699 <cha23215@daum.net>
2024-10-14 11:13:55 +0900

    1113

--- src/main/java/org/zerock/api1014/product/repository/search/ProductSearchImpl.java
+++ src/main/java/org/zerock/api1014/product/repository/search/ProductSearchImpl.java
@@ -1,10 +1,14 @@
 package org.zerock.api1014.product.repository.search;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.api1014.product.domain.Product;
+import org.zerock.api1014.product.domain.QProduct;
+import org.zerock.api1014.product.domain.QReview;
 
 @Log4j2
 public class ProductSearchImpl extends QuerydslRepositorySupport implements ProductSearch {
@@ -17,6 +21,25 @@ public class ProductSearchImpl extends QuerydslRepositorySupport implements Prod
     public Page<Product> list(Pageable pageable) {
 
         log.info("list ---------------!!");
+
+        QProduct product = QProduct.product;
+        QReview review = QReview.review;
+
+        JPQLQuery<Product> query = from(product);
+        query.leftJoin(review).on(review.product.eq(product));
+        query.groupBy(product);
+
+        //페이징 처리 정렬처리
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        JPQLQuery<Tuple> tupleQuery =
+                query.select(
+                        product,
+                        review.count()
+                );
+
+        tupleQuery.fetch();
+
         return null;
     }
 }
