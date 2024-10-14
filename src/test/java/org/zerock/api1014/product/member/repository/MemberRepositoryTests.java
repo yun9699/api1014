6e47deadd559dd4d412a2e174c17377e0d933d88
yun9699 <cha23215@daum.net>
2024-10-14 14:01:26 +0900

    1400 테스트 코드 실행후  member_entity í테ìy 테이블 확 ì인

--- src/test/java/org/zerock/api1014/product/member/repository/MemberRepositoryTests.java
+++ src/test/java/org/zerock/api1014/product/member/repository/MemberRepositoryTests.java
@@ -1,13 +1,41 @@
 package org.zerock.api1014.product.member.repository;
 
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.api1014.member.domain.MemberEntity;
+import org.zerock.api1014.member.domain.MemberRole;
+import org.zerock.api1014.member.repository.MemberRepository;
 
-@DataJpaTest
+
+@SpringBootTest
 @Log4j2
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MemberRepositoryTests {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Commit
+    public void testDummies(){
+
+        for (int i = 1; i <= 10; i++) {
+            MemberEntity member = MemberEntity.builder()
+                    .email("user"+i+"@aaa.com")
+                    .pw(passwordEncoder.encode("1111"))
+                    .role( i < 8 ? MemberRole.USER: MemberRole.ADMIN)
+                    .build();
+            memberRepository.save(member);
+        }
+    }
 }
