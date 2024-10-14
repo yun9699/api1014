ec4b8ebef4441a14d39de3e297b5f813dde7444a
yun9699 <cha23215@daum.net>
2024-10-14 09:36:29 +0900

    Initial commit

---
+++ src/main/java/org/zerock/api1014/Api1014Application.java
@@ -0,0 +1,13 @@
+package org.zerock.api1014;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api1014Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api1014Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/api1014/Api1014ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.api1014;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api1014ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
