3ea1b3db4b090b196a16719860273661a01f0946
yun9699 <cha23215@daum.net>
2024-10-14 13:52:33 +0900

    1352

---
+++ src/main/java/org/zerock/api1014/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package org.zerock.api1014.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.api1014.member.domain.MemberEntity;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+
+
+}
---
+++ src/test/java/org/zerock/api1014/product/member/repository/MemberRepositoryTests.java
@@ -0,0 +1,13 @@
+package org.zerock.api1014.product.member.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@Log4j2
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MemberRepositoryTests {
+
+
+}
